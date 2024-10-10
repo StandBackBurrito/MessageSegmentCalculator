5e39eee6d5ff6fd8923df10446a0687b44c2d305
Justin Patterson <justin@addmembers.com>
2024-10-10 16:33:46 -0500

    added tests from methods.test.ts

---
+++ test/MessageSegmentCalculator.Tests/SegmentedMessageTests.cs
@@ -0,0 +1,19 @@
+using FluentAssertions;
+
+public class SegmentedMessageTests
+{
+    [Fact]
+    public void GetNonGsmCharacters_ReturnsExpectedCharacters()
+    {
+        // Arrange
+        var testMessage = "más";
+        var segmentedMessage = new SegmentedMessage(testMessage);
+
+        // Act
+        var result = segmentedMessage.GetNonGsmCharacters();
+
+        // Assert
+        var expected = new[] { "á" };
+        result.Should().BeEquivalentTo(expected);
+    }
+}
